5a321e03fab9fcf60915bedbc52b27582409e432
shahrodkh <shahrodkh@gmail.com>
2017-03-07 16:13:38 -0800

    refactored greyscale function

--- wasmMath.js
+++ wasmMath.js
@@ -9,10 +9,21 @@ function loadWASM () {
             var script = document.createElement('script');
             script.src = 'cMath.js';
             script.onload = function () {
+              console.log('Module', Module);
               console.log('Emscripten boilerplate loaded.');
-              cMath['double'] = Module.cwrap('doubler', 'number', ['number']);
-              cMath['fib'] = Module.cwrap('fib', 'number', ['number']);
-              cMath['manipArr'] = Module.cwrap('manipArr', null, ['number', 'number']);
+              // cMath['double'] = Module.cwrap('doubler', 'number', ['number']);
+              // cMath['fib'] = Module.cwrap('fib', 'number', ['number']);
+              // cMath['manipArr'] = Module.cwrap('manipArr', null, ['number', 'number']);
+              cMath['doubler'] = _doubler;
+              cMath['fib'] = _fib;
+              cMath['greyScale'] = function(array) {
+                mem = _malloc(array.length);
+                HEAPU8.set(array, mem);
+                Module._greyScale(mem, array.length);
+                const greyScaled = HEAPU8.subarray(mem, mem + array.length);
+                _free(mem);
+                return greyScaled;
+              };
               resolve(cMath);
             };
             document.body.appendChild(script);
