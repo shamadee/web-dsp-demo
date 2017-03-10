32cef39a7cd758437be5f7b2bb614c04ed2db8c7
Matthias Wagner <matzewagner@gmail.com>
2017-03-09 16:46:03 -0800

    added basic CPP testing

--- compileWASM.sh
+++ compileWASM.sh
@@ -9,7 +9,7 @@ source "$EMSDK_ENV"
 # add exported C/C++ functions here
 CPP_FUNCS="['_manipArr', '_manipSingle', '_convFilter', '_fib', '_doubler', '_greyScale']"
 
-echo "compiling C++ ..."
+echo "compiling C++ to WASM ..."
 emcc -o cMath.js math.cpp -lm -O3 -s WASM=1 \
 -s BINARYEN_IMPRECISE=1 \
 -s EXPORTED_FUNCTIONS="$CPP_FUNCS" \
