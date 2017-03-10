a2f50dc3a5d8791626d055013dbd644619145040
Deep Pulusani <sdeep27@yahoo.com>
2017-03-10 15:06:31 -0800

    adding pure js filters

--- demo2.js
+++ demo2.js
@@ -18,6 +18,9 @@ vid.addEventListener("loadedmetadata", function() {
   ch = canvas.clientHeight;
 });
 
+createStats();
+addButtons();
+
 //javascript video
 var vid2 = document.getElementById('v2');
 var canvas2 = document.getElementById('c2');
@@ -36,7 +39,14 @@ function draw() {
   context.drawImage(vid, 0, 0);
   let pixels = context.getImageData(0, 0, vid.videoWidth, vid.videoHeight);
   let t0 = performance.now();
-  pixels.data.set(m.greyScale(pixels.data));
+  if (filter === 'Grayscale') pixels.data.set(m.greyScale(pixels.data));
+  if (filter === 'Brighten') pixels.data.set(m.brighten(pixels.data));
+  if (filter === 'Invert') pixels.data.set(m.invert(pixels.data));
+  if (filter === 'Noise') pixels.data.set(m.noise(pixels.data));
+  if (filter === 'Sunset') pixels.data.set(m.edgeManip(pixels.data, 4, cw)); //red cyan
+  if (filter === 'Analog TV') pixels.data.set(m.edgeManip(pixels.data, 7, cw)); //dots
+  if (filter === 'Emboss') pixels.data.set(m.edgeManip(pixels.data, 1, cw)); //emboss
+  if (filter === 'Super Edge') pixels.data.set(m.convFilt(pixels.data, 720, 486));
   let t1 = performance.now();
   context.putImageData(pixels, 0, 0);
   animation = requestAnimationFrame(draw); 
@@ -56,7 +66,11 @@ function draw2() {
   context2.drawImage(vid2, 0, 0);
   let pixels2 = context2.getImageData(0, 0, vid2.videoWidth, vid2.videoHeight);
   let t2 = performance.now();
-  pixels2.data.set(jsGreyScale(pixels2.data));
+  //pixels2.data.set(jsGreyScale(pixels2.data));
+  if (filter === 'Grayscale') pixels2.data.set(jsGreyScale(pixels2.data));
+  if (filter === 'Brighten') pixels2.data.set(jsBrighten(pixels2.data));
+  if (filter === 'Invert') pixels2.data.set(jsInvert(pixels2.data));
+  if (filter === 'Noise') pixels2.data.set(jsNoise(pixels2.data));
   let t3 = performance.now();
   context2.putImageData(pixels2, 0, 0);
   //adding lines to stats stream
@@ -70,21 +84,7 @@ function draw2() {
   }
 }
 
-function jsGreyScale(data) {
-    for (let i = 0; i < data.length; i += 4) {
-      let r = data[i];
-      let g = data[i+1];
-      let b = data[i+2];
-      let a = data[i+3];
-      let brightness = (r*.21+g*.72+b*.07);
 
-      data[i] = r;
-      data[i+1] = r;
-      data[i+2] = r;
-      data[i+3] = a;
-    }
-    return data;
-}
 
 function createStats() {
   let smoothie = new SmoothieChart({
@@ -124,4 +124,16 @@ function createStats() {
     }
   );
 }
-createStats();
+
+function addButtons (filtersArr) {
+  let filters = ['Normal', 'Grayscale', 'Brighten', 'Invert', 'Noise', 'Sunset', 'Analog TV', 'Emboss', 'Super Edge']
+  let buttonDiv = document.createElement('div');
+  buttonDiv.id = 'buttons';
+  document.body.appendChild(buttonDiv);
+  for (let i = 0; i < filters.length; i++) {
+    let button = document.createElement('button');
+    button.innerText = filters[i];
+    button.addEventListener('click', () => filter = filters[i]);
+    buttonDiv.appendChild(button);
+  }
+}
