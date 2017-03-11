e650a5fa702a846ff90c5ded6a22560d1d0838f6
shahrodkh <shahrodkh@gmail.com>
2017-03-11 14:21:08 -0800

    added flag to allow memory growth

--- compileWASM.sh
+++ compileWASM.sh
@@ -3,13 +3,14 @@
 # use path to emsdk folder, relative to this directory
 BASEDIR="./../emsdk"
 BASEDIRSHA="./../../../Documents/emsdk"
-EMSDK_ENV=$(find "$BASEDIR" -type f -name "emsdk_env.sh")
+EMSDK_ENV=$(find "$BASEDIRSHA" -type f -name "emsdk_env.sh")
 source "$EMSDK_ENV"
 
 # add exported C/C++ functions here
-CPP_FUNCS="['_manipArr', '_manipSingle', '_convFilter', '_gaussFilter', '_fib', '_doubler', '_greyScale', '_brighten', '_invert', '_noise', '_edgeManip']"
+CPP_FUNCS="['_manipArr', '_manipSingle', '_sobelFilter', '_convFilter', '_fib', '_doubler', '_greyScale', '_brighten', '_invert', '_noise', '_edgeManip']"
 
 echo "compiling C++ to WASM ..."
 emcc -o cMath.js math.cpp -lm -O3 -s WASM=1 \
 -s BINARYEN_IMPRECISE=1 \
 -s EXPORTED_FUNCTIONS="$CPP_FUNCS" \
+-s ALLOW_MEMORY_GROWTH=1 \
