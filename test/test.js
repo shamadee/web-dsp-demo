f225b5f23129f60ce9046c116bd45151b9183647
Matthias Wagner <matzewagner@gmail.com>
2017-03-13 19:44:31 -0700

    changed folder structure

--- test/test.js
+++ test/test.js
@@ -4,15 +4,6 @@ const Browser = require('zombie');
 
 Browser.localhost('example.com', 3000);
 
-describe('Testing some stuff here', function () {
-  it('should return a result', function () {
-    expect(2).to.equal(2);
-  });
-  it('should return a correct result', function () {
-    expect(3).to.equal(3);
-  });
-});
-
 describe('Should get to the page and', function () {
   let app, browser;
   before(function() {
