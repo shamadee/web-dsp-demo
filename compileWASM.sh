946ed0af063c3d89cfd0b34930cca5857b8fb697
shahrodkh <shahrodkh@gmail.com>
2017-03-18 10:30:01 -0700

    added backup JS funcs if wasm not supported. refactored cpp funcs

--- compileWASM.sh
+++ compileWASM.sh
@@ -3,23 +3,22 @@
 # use path to emsdk folder, relative to this directory
 BASEDIR="./../emsdk"
 BASEDIRSHA="./../../../Documents/emsdk"
-EMSDK_ENV=$(find "$BASEDIR" -type f -name "emsdk_env.sh")
+EMSDK_ENV=$(find "$BASEDIRSHA" -type f -name "emsdk_env.sh")
 source "$EMSDK_ENV"
 
 # add exported C/C++ functions here
-CPP_FUNCS="[ 
- '_sobelFilter', 
- '_convFilter', 
- '_doubler', 
- '_grayScale', 
- '_brighten', 
- '_invert', 
- '_noise', 
- '_edgeManip' 
+CPP_FUNCS="[
+ '_grayScale',
+ '_brighten',
+ '_invert',
+ '_noise',
+ '_multiFilter',
+ '_sobelFilter',
+ '_convFilter',
 ]" 
 
 echo "compiling C++ to WASM ..."
-emcc -o ./lib/webdsp_c.js ./cpp/webdsp.cpp -lm -O3 -s WASM=1 \
+emcc -o ./lib/webdsp_c.js ./cpp/webdsp.cpp -lm -s WASM=1 \
 -s BINARYEN_IMPRECISE=1 \
 -s EXPORTED_FUNCTIONS="$CPP_FUNCS" \
 -s ALLOW_MEMORY_GROWTH=1 \
