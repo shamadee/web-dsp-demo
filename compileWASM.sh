b0090fb984dddb892aaa62ff31a6a4da195e34db
shahrodkh <shahrodkh@gmail.com>
2017-03-18 10:33:30 -0700

    added -O3 optimization flag to shell script

--- compileWASM.sh
+++ compileWASM.sh
@@ -18,7 +18,7 @@ CPP_FUNCS="[
 ]" 
 
 echo "compiling C++ to WASM ..."
-emcc -o ./lib/webdsp_c.js ./cpp/webdsp.cpp -lm -s WASM=1 \
+emcc -o ./lib/webdsp_c.js ./cpp/webdsp.cpp -lm -O3 -s WASM=1 \
 -s BINARYEN_IMPRECISE=1 \
 -s EXPORTED_FUNCTIONS="$CPP_FUNCS" \
 -s ALLOW_MEMORY_GROWTH=1 \
