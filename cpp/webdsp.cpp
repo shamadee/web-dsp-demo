04a3bb7fc77b3c14b36f9d7a10f420316b414ba9
shahrodkh <shahrodkh@gmail.com>
2017-03-21 22:01:55 -0700

    fixed convolution filter, added clamping to ouput

--- cpp/webdsp.cpp
+++ cpp/webdsp.cpp
@@ -111,7 +111,7 @@ extern "C" {
       }
     }
   }
-  void convFilter (float* data, int width, int height, float* kern, int kWidth, int kHeight, float divisor, double bias, int count) {
+  void convFilter (float* data, int width, int height, float* kern, int kWidth, int kHeight, float divisor, float bias, int count) {
     float r, g, b;
     int yy, xx, imageOffset, kernelOffset, pix; 
     int kCenterY = floor(kHeight / 2);
@@ -135,9 +135,9 @@ extern "C" {
             }
           }
           pix = (width * y + x) * 4;
-          data[pix + 0] = r / divisor;
-          data[pix + 1] = g / divisor;
-          data[pix + 2] = b / divisor;
+          data[pix + 0] =  ((r / divisor)>255.0) ? 255.0 : ((r / divisor)<0.0) ? 0.0 : r / divisor;
+          data[pix + 1] =  ((g / divisor)>255.0) ? 255.0 : ((g / divisor)<0.0) ? 0.0 : g / divisor;
+          data[pix + 2] =  ((b / divisor)>255.0) ? 255.0 : ((b / divisor)<0.0) ? 0.0 : b / divisor;
         }
       }
     }
