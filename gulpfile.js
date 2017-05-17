32273ad49c5a342776a685dd09e47ffe5ef985a6
krzysztof-grzybek <krzysztof.grzybek89@gmail.com>
2017-05-17 22:36:43 +0200

    basic windows support

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const bs = require('browser-sync').create();
 const child = require('child_process');
 const exec = require('child_process').exec;
+const fs = require('fs');
 
 gulp.task('default', ['server', 'recompile', 'browser-sync']);
 
@@ -10,8 +11,15 @@ gulp.task('server', () => {
 });
 
 gulp.task('recompile', (cb) => {
-  exec('. ./compileWASM.sh', (err, stdout, stderr) => {
-    // console.log(stdout);
+  const compileCommand = getCompileCommand();
+  exec(compileCommand, (err, stdout, stderr) => {
+
+    if (isWin()) {
+      const data = fs.readFileSync('lib/webdsp_c.js', 'utf8');
+      const newData = data.replace(/else\{doRun\(\)\}/g, '$&script.dispatchEvent(doneEvent);');
+      fs.writeFileSync('lib/webdsp_c.js', newData);
+    }
+
     console.log(stderr);
     cb(err);
     bs.reload();
@@ -29,3 +37,15 @@ gulp.watch('cpp/webdsp.cpp', ['recompile']);
 gulp.watch(['demo.js', 'webdsp.js', 'index.html', 'style.css', 'compileWASM.sh'], () => {
   bs.reload();
 });
+
+function getCompileCommand() {
+  if (isWin()) {
+    return 'compileWASM.bat';
+  } else {
+    return '. ./compileWASM.sh';
+  }
+}
+
+function isWin() {
+  return /^win/.test(process.platform);
+}
